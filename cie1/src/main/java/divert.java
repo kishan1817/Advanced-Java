e542e5231a484cc2a94aa849b8997d5baca386a4
kishan joshi <71633570+kishan1817@users.noreply.github.com>
2020-09-21 18:11:18 +0530

    Added cie 1

---
+++ cie1/src/main/java/divert.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Kishan Joshi
+ */
+public class divert extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req,HttpServletResponse res) throws IOException{
+        String txtsearch;
+    String sname=req.getParameter("txtsearch");
+    
+       if(sname.equals("shopping") || sname.equals("movies") || sname.equals("online games") )
+       {
+           res.sendError(400, "SC_BAD_REQUEST");
+       }
+       else{
+           HttpSession session=req.getSession();  
+            session.setAttribute("searchtxt",sname); 
+            res.sendRedirect("divert.html");
+       }
+        //rintWriter pw=res.getWriter();
+       // pw.println("Diverted");
+       
+        
+    }
+   
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
